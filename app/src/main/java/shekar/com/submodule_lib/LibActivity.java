9287b4f2d895b565a9a402a46cba1ac0fdcc0f7e
Shekar <shekarrex@gmail.com>
2016-07-30 12:37:20 +0530

    Initial commit

---
+++ app/src/main/java/shekar/com/submodule_lib/LibActivity.java
@@ -0,0 +1,13 @@
+package shekar.com.submodule_lib;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+    }
+}
